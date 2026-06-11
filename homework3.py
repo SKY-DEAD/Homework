2cf45c8442363dea83db271aaa5b145bfbabda81
Nick <Boevayaclassica13@gmail.com>
2026-06-11 14:19:14 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,22 @@
+# num1 = int(input("Ведите число: "))
+# num2 = int(input("Ведите число: "))
+# total_sum = 0
+# for i in range(num1,num2+1):
+#     total_sum += i
+# print(total_sum)
+#
+# num = int(input("Ведите число: "))
+# for i in range(1,num+1):
+#     if num % i == 0:
+#         print(i)
+
+# num1 = int(input("Ведите число: "))
+# num2 = int(input("Ведите число: "))
+# for i in range(num1, 0,-1):
+#     if num1 % i == 0 and num2 % i == 0:
+#         print(i)
+#         break
+
+number = input("Ведите число: ")
+print(" количество цифр в числе:",len(number))
+
